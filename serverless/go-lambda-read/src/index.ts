c51b93ad78e0919b9350def8926d17f430980289
Zhao Wei Liew <zhaoweiliew@gmail.com>
2021-03-06 23:54:26 +0800

    Complete read lambda

--- serverless/go-lambda-read/src/index.ts
+++ serverless/go-lambda-read/src/index.ts
@@ -1,4 +1,4 @@
-import AWS from "aws-sdk";
+import { DynamoDBClient, GetItemCommand } from "@aws-sdk/client-dynamodb";
 
 const {
   AWS_REGION,
@@ -8,23 +8,32 @@ const {
   TO_ATTR,
 } = process.env as { [key: string]: string };
 
-const ddb = new AWS.DynamoDB({ region: AWS_REGION });
+const client = new DynamoDBClient({ region: AWS_REGION });
 
 exports.handler = async (event: any) => {
-  const { [FROM_ATTR]: from } = event.pathParameters;
-  const params = {
-    TableName: TABLE_NAME,
-    Key: {
-      [PARTITION_KEY]: { S: from },
-    },
-    ProjectionExpression: TO_ATTR,
-  };
-  const data = await ddb.getItem(params).promise();
-  if (!data || !data.Item || !data.Item[TO_ATTR] || !data.Item[TO_ATTR].S) {
+  try {
+    const { [FROM_ATTR]: from } = event.pathParameters;
+    const params = {
+      TableName: TABLE_NAME,
+      Key: {
+        [PARTITION_KEY]: { S: from },
+      },
+      ProjectionExpression: TO_ATTR,
+    };
+    const command = new GetItemCommand(params);
+    const data = await client.send(command);
+    if (!data || !data.Item || !data.Item[TO_ATTR] || !data.Item[TO_ATTR].S) {
+      return {
+        statusCode: 404,
+        body: "Not Found",
+      };
+    }
+    return data.Item[TO_ATTR].S;
+  } catch (err) {
+    console.error(err);
     return {
-      statusCode: 404,
-      body: "Not Found",
+      statusCode: 400,
+      body: "Bad Request",
     };
   }
-  return data.Item[TO_ATTR].S;
 };
